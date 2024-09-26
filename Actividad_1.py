dc4f7137b291e71592225b4d5cff754827e19522
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-25 20:07:03 -0600

    Add files via upload

---
+++ Actividad_1.py
@@ -0,0 +1,6 @@
+print ("Lazo_Jimenez_Angel_Jesus_3W")
+num1 = float(input("ingresar primer numero: ")) #Pide al usuario el primer numero
+num2 = float(input("ingresar segundo numero:")) #Pide al usuario el segundo numero
+num3 = float(input("ingresar tercer numero: ")) #Pide al usuario el tercer numero
+resultado = num1 * num2 * num3 #Muestra la multiplicacion de los tres numeros
+print(f"La multiplicacion de {num1} * {num2} * {num3} es: {resultado}") #Muestra el resultado de la multiplicacion
